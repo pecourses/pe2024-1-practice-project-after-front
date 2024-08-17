31b0512aa589a5dc1ccf0bf49eb0bbd95cbb3fc1
Viktoriia <freshitpte@gmail.com>
2024-08-17 18:14:36 +0300

    Initial commit

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
+import styles from './Spinner.module.sass';
+
+const override = { borderСolor: '#46568a' };
+
+const SpinnerLoader = () => (
+  <div className={styles.loaderContainer}>
+    <ClipLoader cssOverride={override} size={50} color="#46568a" />
+  </div>
+);
+
+export default SpinnerLoader;
