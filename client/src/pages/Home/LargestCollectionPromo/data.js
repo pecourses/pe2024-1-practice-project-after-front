f5b108d661e63754626ecfa92049e60646bd6a02
Ivan Horevych <gorevich@outlook.com>
2024-08-25 00:16:12 +0300

    project_task_1

---
+++ client/src/pages/Home/LargestCollectionPromo/data.js
@@ -0,0 +1,134 @@
+import CONSTANTS from './../../../constants';
+
+const { STATIC_IMAGES_PATH } = CONSTANTS;
+
+const data = {
+  TopCategories: [
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon1.svg`,
+      bgColor: '#f7f5f3',
+      text: 'Popular Brandable Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon2.svg`,
+      bgColor: '#fcebe6',
+      text: 'Short Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon3.svg`,
+      bgColor: '#e8f7f7',
+      text: '3 Letter Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon4.svg`,
+      bgColor: '#e8f7f7',
+      text: '4 Letter Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon5.svg`,
+      bgColor: '#ecf5e6',
+      text: '5 Letter Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon6.svg`,
+      bgColor: '#f9f2e7',
+      text: 'One Word Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon7.svg`,
+      bgColor: '#fcebe6',
+      text: 'Ultra Premium Domains',
+    },
+    {
+      icon: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopCategories/nh-collection-icon8.svg`,
+      bgColor: '#f7f5f3',
+      text: 'On Sale',
+    },
+  ],
+  TopIndustries: [
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/fashion-clothing-750-04.webp`,
+      bgColor: '#f7f5f3',
+      text: 'Fashion & Clothing Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/beauty-cosmetics-750-33.webp`,
+      bgColor: '#fcebe6',
+      text: 'Beauty & Cosmetics Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/Agency-750-16.webp`,
+      bgColor: '#e8f7f7',
+      text: 'Agency & Consulting Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/finance-750-8.webp`,
+      bgColor: '#e8f7f7',
+      text: 'Finance Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/food-beverage-750-17.webp`,
+      bgColor: '#ecf5e6',
+      text: 'Food & Beverage Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/health-medical-750-11.webp`,
+      bgColor: '#f9f2e7',
+      text: 'Health & Medical Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/e-commerce-retail-750-28.webp`,
+      bgColor: '#fcebe6',
+      text: 'E-Commerce & Retail Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIndustries/technology-750-31.webp`,
+      bgColor: '#f7f5f3',
+      text: 'Tech, Internet, Software Domains',
+    },
+  ],
+  TopIdeas: [
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-14.webp`,
+      bgColor: '#f7f5f3',
+      text: 'Smart Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/Agency-750-1.webp`,
+      bgColor: '#fcebe6',
+      text: 'Luxury Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-11.webp`,
+      bgColor: '#e8f7f7',
+      text: 'Style Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-25.webp`,
+      bgColor: '#e8f7f7',
+      text: 'Care Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-13.webp`,
+      bgColor: '#ecf5e6',
+      text: 'Wish Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-20.webp`,
+      bgColor: '#f9f2e7',
+      text: 'Trend Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-15.webp`,
+      bgColor: '#fcebe6',
+      text: 'Pure Domains',
+    },
+    {
+      image: `${STATIC_IMAGES_PATH}home/LargestCollectionPromo/TopIdeas/other-750-30.webp`,
+      bgColor: '#f7f5f3',
+      text: 'Grow Domains',
+    },
+  ],
+};
+
+export default data;
